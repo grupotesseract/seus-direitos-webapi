ff11b1e8a1d71c4b7a5d90621593a706295e3b23
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2018-09-09 21:20:26 -0300

    Esqueleto avançado

--- resources/views/carteirinha/index.blade.php
+++ resources/views/carteirinha/index.blade.php
@@ -1,39 +1,41 @@
 <style type="text/css">
-	
-
+	.h3-texto {
+		font-size: 15px;
+	}
 </style>
 
 @extends('layout-welcome.head')
 
-<div class="container"> 
+<div class="container" style="margin-top: 20px; margin-left: 50px"> 
+    
     <div class="section">
         <div class="row">
 	    	<img src="/img/logo-seusindicato.jpg" width="40%" height="40%" class="img-responsive" align="left">
 	    </div>
 	</div>
 	
-    <div class="section">
+    <div class="section" style="margin-top: 40px; margin-left: 10px">
         <div class="row">
 	        <div class="col-3">
-				<img src="{{ $carteirinha->sindicato->logo->url_cloudinary }}" width="20%" height="20%" class="img-responsive" align="left">	
+				<img src="{{ $carteirinha->sindicato->logo->url_cloudinary }}" width="150px" height="350px" class="img-responsive" align="left">	
 			</div>
 
 			<div class="col-9">
-				<h3>{{ $carteirinha->sindicato->nome }}</h3>			
+				<h3 class="h3-texto" >{{ $carteirinha->sindicato->nome }}</h3>			
 			</div>
 		</div>
     </div>
 
-    <div class="section">
+    <div class="section" style="margin-left: 10px">
         <div class="row">
-			<h3>Associado: {{ $carteirinha->name }}</h3>			
-			<h3>RG: {{ $carteirinha->rg }}</h3>			
+			<h3 class="h3-texto">Associado: {{ $carteirinha->name }}</h3>			
+			<h3 class="h3-texto">RG: {{ $carteirinha->rg }}</h3>			
 		</div>
     </div>
 
-    <div class="section">
+    <div class="section" style="margin-top: -20px">
         <div class="row">
-			<h3>Instituicão: {{ $carteirinha->instituicao->nomecompleto }}</h3>			
+			<h3 class="h3-texto" align="center">Instituicão: {{ $carteirinha->instituicao->nomecompleto }}</h3>			
 		</div>
     </div>
 
