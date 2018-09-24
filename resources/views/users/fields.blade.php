d9327e009283beadbd05943091af3c24e5a728fe
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2018-09-24 15:22:48 -0300

    RG

--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -16,6 +16,12 @@
     {!! Form::password('password', ['class' => 'form-control']) !!}
 </div>
 
+<!-- Name Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('rg', 'RG:') !!}
+    {!! Form::text('rg', null, ['class' => 'form-control']) !!}
+</div>
+
 @if (\Request::segment(2) == 'administradores')
 
     {!! Form::hidden('role', 'superadmin') !!}
