9d905616a698b597038d634eb676235c32de6849
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-02-26 20:22:02 -0300

    Acrescentando coluna em Users

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,7 @@ class User extends Authenticatable
         'cidade_id',
         'instituicao_id',
         'rg',
+        'celular'
     ];
 
     /**
---
+++ database/migrations/2019_02_26_201928_criar_coluna_celular_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarColunaCelularUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('celular')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('celular');
+        });
+    }
+}
