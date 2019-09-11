3a1e32ed6bd10072684ec86e773708002f32e913
Leandro Acquati <leandroacquati@gmail.com>
2019-09-11 18:32:04 -0300

    layout header

--- resources/views/landing-page/home.blade.php
+++ resources/views/landing-page/home.blade.php
@@ -3,8 +3,18 @@
 @section('content')
 <div class="">
     <div class="header">
-        <div></div>
-        <div></div>
+        <div>
+            <img src="https://res.cloudinary.com/tesseract/image/upload/v1567982771/seus-direitos/logo-seu-sindicato.png"
+                alt="Logo Seu Sindicato">
+        </div>
+        <div>
+            <h1>O APP</h1>
+            <h2>DO TRABALHADOR</h2>
+            <h3>Todas as informações sobre sua categoria de trabalho, em um mesmo lugar.</h3>
+            <p>Modernize a forma de comunicação entre o Sindicato e os trabalhadores da categoria.</p>
+            <div>Iphone</div>
+            <div>Android</div>
+        </div>
     </div>
     <div class="login">
     </div>
