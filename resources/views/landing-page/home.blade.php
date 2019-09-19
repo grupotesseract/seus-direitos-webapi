7090e1b2d53dd2c9d4159ae4312baf320cc4993d
Leandro Acquati <leandroacquati@gmail.com>
2019-09-19 18:22:52 -0300

    login layout for tablet

--- resources/views/landing-page/home.blade.php
+++ resources/views/landing-page/home.blade.php
@@ -17,7 +17,8 @@
     </div>
 
     <div class="hero-2">
-        <img src="https://res.cloudinary.com/tesseract/image/upload/v1568925429/seus-direitos/mao-app.png" alt="Mockup Seu Sindicato">
+        <img src="https://res.cloudinary.com/tesseract/image/upload/v1568925429/seus-direitos/mao-app.png"
+            alt="Mockup Seu Sindicato">
     </div>
 
     <div class="hero-3">
@@ -32,43 +33,45 @@
 </div>
 
 <div class="login">
-    <h1>AINDA NÃO É<br>
-        CADASTRADO?</h1>
-    <h2>SOLICITE O SEU ACESSO:</h2>
-    <h3>canalseusdireitos@gmail.com</h3>
+    <div class="login-container">
+        <h1>AINDA NÃO É<br>
+            CADASTRADO?</h1>
+        <h2>SOLICITE O SEU ACESSO:</h2>
+        <h3>canalseusdireitos@gmail.com</h3>
 
-    <h2>Já é cadastrado?</h2>
-    <h3>Faça seu login</h3>
-    <form method="post" action="{{ url('/login') }}">
-        {!! csrf_field() !!}
+        <h2>Já é cadastrado?</h2>
+        <h3>Faça seu login</h3>
+        <form method="post" action="{{ url('/login') }}">
+            {!! csrf_field() !!}
 
-        <div class="form-group">
-            @if ($errors->has('email'))
-            <span class="help-block">
-                <strong>{{ $errors->first('email') }}</strong>
-            </span>
-            @endif
-            <input type="email" class="form-control {{ $errors->has('email') ? ' has-error' : '' }}" name="email"
-                value="{{ old('email') }}" placeholder="email">
-        </div>
+            <div class="form-group">
+                @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
+                <input type="email" class="form-control {{ $errors->has('email') ? ' has-error' : '' }}" name="email"
+                    value="{{ old('email') }}" placeholder="email">
+            </div>
 
-        <div class="form-group">
-            @if ($errors->has('password'))
-            <span class="help-block">
-                <strong>{{ $errors->first('password') }}</strong>
-            </span>
-            @endif
-            <input type="password" class="form-control {{ $errors->has('password') ? ' has-error' : '' }}"
-                name="password" placeholder="senha">
-        </div>
+            <div class="form-group">
+                @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
+                <input type="password" class="form-control {{ $errors->has('password') ? ' has-error' : '' }}"
+                    name="password" placeholder="senha">
+            </div>
 
-        <button type="submit" class="submit-button">entrar</button>
+            <button type="submit" class="submit-button">entrar</button>
 
-        <label>lembre-me
-            <input type="checkbox" name="remember">
-            <span class="checkmark"></span>
-        </label>
-    </form>
+            <label>lembre-me
+                <input type="checkbox" name="remember">
+                <span class="checkmark"></span>
+            </label>
+        </form>
+    </div>
 </div>
 
 <div class="menu">
