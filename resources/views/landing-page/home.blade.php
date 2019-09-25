abb075f533ed8a73c6698d21b7137e8057ac6e5c
Leandro Acquati <leandroacquati@gmail.com>
2019-09-25 17:09:42 -0300

    layout header large screens

--- resources/views/landing-page/home.blade.php
+++ resources/views/landing-page/home.blade.php
@@ -1,78 +1,81 @@
 @extends('landing-page.app')
 
 @section('content')
-<div class="logo">
-    <img src="https://res.cloudinary.com/tesseract/image/upload/v1567982771/seus-direitos/logo-seu-sindicato.png"
-        alt="Logo Seu Sindicato">
-</div>
 
-<div class="hero">
-    <div class="hero-1">
-        <h1>O APP</h1>
-        <h2>DO TRABALHADOR</h2>
-        <h3>Todas as informações sobre sua categoria de trabalho, em um mesmo lugar.</h3>
-        <h4>Modernize a forma de comunicação entre o Sindicato e os trabalhadores da categoria.</h4>
-        <div class="button"><i class="fab fa-apple"></i>Iphone</div>
-        <div class="button"><i class="fab fa-android"></i>Android</div>
+<section class="header">
+    <div class="logo">
+        <img src="https://res.cloudinary.com/tesseract/image/upload/v1567982771/seus-direitos/logo-seu-sindicato.png"
+            alt="Logo Seu Sindicato">
     </div>
 
-    <div class="hero-2">
-        <img src="https://res.cloudinary.com/tesseract/image/upload/v1568925429/seus-direitos/mao-app.png"
-            alt="Mockup Seu Sindicato">
+    <div class="hero">
+        <div class="hero-1">
+            <h1>O APP</h1>
+            <h2>DO TRABALHADOR</h2>
+            <h3>Todas as informações sobre sua categoria de trabalho, em um mesmo lugar.</h3>
+            <h4>Modernize a forma de comunicação entre o Sindicato e os trabalhadores da categoria.</h4>
+            <div class="button"><i class="fab fa-apple"></i>Iphone</div>
+            <div class="button"><i class="fab fa-android"></i>Android</div>
+        </div>
+    
+        <div class="hero-2">
+            <img src="https://res.cloudinary.com/tesseract/image/upload/v1568925429/seus-direitos/mao-app.png"
+                alt="Mockup Seu Sindicato">
+        </div>
+    
+        <div class="hero-3">
+            <h3>LEIA NOTÍCIAS</h3>
+            <h3>CONHEÇA BENEFÍCIOS</h3>
+            <h3>FALE COM<br>
+                REPRESENTANTES<br>
+                DA SUA CATEGORIA</h3>
+            <h3>UTILIZE A<br>
+                CARTEIRINHA VIRTUAL</h3>
+        </div>
     </div>
-
-    <div class="hero-3">
-        <h3>LEIA NOTÍCIAS</h3>
-        <h3>CONHEÇA BENEFÍCIOS</h3>
-        <h3>FALE COM<br>
-            REPRESENTANTES<br>
-            DA SUA CATEGORIA</h3>
-        <h3>UTILIZE A<br>
-            CARTEIRINHA VIRTUAL</h3>
+    
+    <div class="login">
+        <div class="login-container">
+            <h1>AINDA NÃO É<br>
+                CADASTRADO?</h1>
+            <h2>SOLICITE O SEU ACESSO:</h2>
+            <h3>canalseusdireitos@gmail.com</h3>
+    
+            <h2>Já é cadastrado?</h2>
+            <h3>Faça seu login</h3>
+            <form method="post" action="{{ url('/login') }}">
+                {!! csrf_field() !!}
+    
+                <div class="form-group">
+                    @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                    @endif
+                    <input type="email" class="form-control {{ $errors->has('email') ? ' has-error' : '' }}" name="email"
+                        value="{{ old('email') }}" placeholder="email">
+                </div>
+    
+                <div class="form-group">
+                    @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                    @endif
+                    <input type="password" class="form-control {{ $errors->has('password') ? ' has-error' : '' }}"
+                        name="password" placeholder="senha">
+                </div>
+    
+                <button type="submit" class="submit-button">entrar</button>
+    
+                <label>lembre-me
+                    <input type="checkbox" name="remember">
+                    <span class="checkmark"></span>
+                </label>
+            </form>
+        </div>
     </div>
-</div>
-
-<div class="login">
-    <div class="login-container">
-        <h1>AINDA NÃO É<br>
-            CADASTRADO?</h1>
-        <h2>SOLICITE O SEU ACESSO:</h2>
-        <h3>canalseusdireitos@gmail.com</h3>
-
-        <h2>Já é cadastrado?</h2>
-        <h3>Faça seu login</h3>
-        <form method="post" action="{{ url('/login') }}">
-            {!! csrf_field() !!}
-
-            <div class="form-group">
-                @if ($errors->has('email'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-                @endif
-                <input type="email" class="form-control {{ $errors->has('email') ? ' has-error' : '' }}" name="email"
-                    value="{{ old('email') }}" placeholder="email">
-            </div>
-
-            <div class="form-group">
-                @if ($errors->has('password'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-                @endif
-                <input type="password" class="form-control {{ $errors->has('password') ? ' has-error' : '' }}"
-                    name="password" placeholder="senha">
-            </div>
-
-            <button type="submit" class="submit-button">entrar</button>
-
-            <label>lembre-me
-                <input type="checkbox" name="remember">
-                <span class="checkmark"></span>
-            </label>
-        </form>
-    </div>
-</div>
+</section>
 
 <div class="menu">
     <img src="https://res.cloudinary.com/tesseract/image/upload/v1567982771/seus-direitos/home-convencoes-coletivas.png"
