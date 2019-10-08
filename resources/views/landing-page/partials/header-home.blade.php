89f96a449d1db4a565f14dd164cde4bbc6cb0915
Leandro Acquati <leandroacquati@gmail.com>
2019-10-08 01:07:37 -0300

    add header home mobile

--- resources/views/landing-page/partials/header-home.blade.php
+++ resources/views/landing-page/partials/header-home.blade.php
@@ -1,11 +1,11 @@
-<div class="logo">
+<div class="logo-home">
 	<div class="content">
 		<img src="https://res.cloudinary.com/tesseract/image/upload/v1567982771/seus-direitos/logo-seu-sindicato.png"
 			alt="Logo Seu Sindicato">
 	</div>
 </div>
 
-<div class="login">
+<div class="login-home">
 	<div class="content">
 		<div class="login-1">
 			<h2>AINDA NÃO É<br>
@@ -51,5 +51,39 @@
 				</label>
 			</form>
 		</div>
+	</div>
+</div>
+
+<div class="hero-home">
+	<div class="content">
+			<div class="hero-1">
+				<h1>O APP</h1>
+				<h2>DO TRABALHADOR</h2>
+				<h3>Todas as informações sobre<br>
+					sua categoria de trabalho,<br>
+					em um mesmo lugar.</h3>
+				<h4>Modernize a forma de comunicação entre o<br>
+					Sindicato e os trabalhadores da categoria.</h4>
+				<div class="button"><i class="fab fa-apple"></i>Iphone</div>
+				<div class="button"><i class="fab fa-android"></i>Android</div>
+			</div>
+		
+			{{-- <div class="hero-2">
+				<img src="https://res.cloudinary.com/tesseract/image/upload/v1568925429/seus-direitos/mao-app.png"
+					alt="Mockup Seu Sindicato">
+			</div> --}}
+		
+			<div class="hero-3">
+				<h3>VEJA AS CONVENÇÕES<br>
+				COLETIVAS DA SUA<br>
+				CATEGORIA</h3>
+				<h3>LEIA NOTÍCIAS</h3>
+				<h3>CONHEÇA BENEFÍCIOS</h3>
+				<h3>FALE COM<br>
+					REPRESENTANTES<br>
+					DA SUA CATEGORIA</h3>
+				<h3>UTILIZE A<br>
+					CARTEIRINHA VIRTUAL</h3>
+			</div>
 	</div>
 </div>
