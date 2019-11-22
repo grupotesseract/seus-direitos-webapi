81826b834f988453cd8e2c30926eb32aa5ba2867
Leandro Acquati <leandroacquati@gmail.com>
2019-11-22 14:24:57 -0300

    add header convenções coletivas

--- resources/views/convencaos/indexpublico.blade.php
+++ resources/views/convencaos/indexpublico.blade.php
@@ -1,73 +1,45 @@
-@extends('layout-welcome.head')
+@extends('landing-page.partials.app')
 
-<style>
-    
-    .filme {
-        margin-top: 20px;
-        margin-bottom: 20px;
-        padding-top: 30px;
-        padding-bottom: 30px;
-        border-bottom: 1px solid gray;
-    }
+@section('css')
+<link href="{{ asset('css/main.css') }}" rel="stylesheet">
+@endsection
 
-    
+@section('content')
 
-    .filme h2, .filme h3 {
-        color: #6c6c6c;
-        text-transform: uppercase;
-    }
-
-    .img-responsive, .thumbnail > img, .thumbnail a > img, .carousel-inner > .item > img, .carousel-inner > .item > a > img {
-        display: block;
-        max-width: 100%;
-        height: auto;
-        horizontal-align: middle;
-
-    }
-
-    .logo {
-        margin: 0 auto;
-    }
-
-    .text-center {
-        text-align: center;
-    }
-
-    p {
-        margin: 0 0 10px;
-    }
-
-    .section {
-        position: relative;
-    }
-
-    img {
-        vertical-align: middle;
-    }
-
-    body {
-        font-family: 'Hind', sans-serif;
-    }
-    
-</style>
-
-
-<!-- INCLUIR CABEÇALHO DO ALAMEDA -->
-<!-- AJUSTAR TAMANHO DA IMAGEM -->
-<!-- TRADUZIR LABELS -->
-
-<div class="container">
-    
-    @foreach ($convencoes as $convencao)
-        <div class="row filme">
-
-            <div class="col-lg-12">
-                <h2>{{$convencao->resumo}}</h2>    
-                <a class="btn btn-primary pull-left" href="{{ url('detalhaconvencao/'.$convencao->id) }}">Visualizar</a>    
-            </div>
-
-        </div>
-    @endforeach
-
+<div class="app-header">
+	<div>
+		<h4>Sindicato dos Auxiliares Administrativos Escolar de Bauru e Região</h4>
+	</div>
 </div>
 
+<div class="convencoes-coletivas-header">
+	<div>
+		<div>			
+		</div>
+
+		<div>
+			<h4>CONVENÇÃO COLETIVA DE TRABALHO</h4>
+			<h4>OS SEUS DIREITOS ESTÃO AQUI</h4>
+			<hr>
+		</div>
+	</div>
+</div>
+
+<div class="app-pesquisar"></div>
+
+<div class="convencoes-coletivas">
+	<div>
+		@foreach ($convencoes as $convencao)
+		<div class="">
+
+			<div class="">
+				<h4>{{$convencao->resumo}}</h4>
+				<a class="" href="{{ url('detalhaconvencao/'.$convencao->id) }}"></a>
+			</div>
+
+		</div>
+		@endforeach
+	</div>
+</div>
+
+@endsection
