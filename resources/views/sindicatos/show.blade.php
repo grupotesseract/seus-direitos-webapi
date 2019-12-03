a8eace1099db557e37477620e4a22b63c5891034
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-12-03 11:31:07 -0300

    Incluindo botão de visualização de associados de um sindicato

--- resources/views/sindicatos/show.blade.php
+++ resources/views/sindicatos/show.blade.php
@@ -4,15 +4,17 @@
     <section class="content-header">
         <h1>
             Sindicato
-        </h1>
+				</h1>
+				<a href={{ $sindicato->id.'/usuarios' }} class="btn btn-primary">Visualizar Associados</a>
     </section>
     <div class="content">
         <div class="box box-primary">
             <div class="box-body">
-                <div class="row" style="padding-left: 20px">
+								<div class="row" style="padding-left: 20px">
                     @include('sindicatos.show_fields')
                     <a href="{!! route('sindicatos.index') !!}" class="btn btn-default">Voltar</a>
-                </div>
+								</div>
+								
             </div>
         </div>
     </div>
